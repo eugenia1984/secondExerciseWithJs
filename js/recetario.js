aab2cbbcc71481ee1eb432fc941cb1007b167dc8
Eugenia <costamariaeugenia1@gmail.com>
2021-01-15 21:11:32 -0300

    add files via upload

---
+++ js/recetario.js
@@ -0,0 +1,12 @@
+var numero1 = prompt ('Elegí un primer número entero positivo')
+var numero2 = prompt ('Elegí un segundo número entero positivo')
+alert ('Tus números elegidos son: ' + numero1 + ' y ' + numero2)
+
+var suma = parseInt(numero1) + parseInt(numero2)
+var resta = parseInt(numero1) - 1 
+var multiplicacion = parseInt(numero2) * 2
+var division = 10 / 2
+
+var seriesTv = ['NCSI', 'The Mentalist', 'House', 'Zumbo Just Desserts', 'Property Brothers']
+seriesTv.sort()
+
